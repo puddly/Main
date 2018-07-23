f27e684d2fb4901149b6f5e87317cd2c095f323b
puddly <32534428+puddly@users.noreply.github.com>
2018-07-23 17:51:25 -0400

    Added support for Cairo in the build script

--- v4l2rtspserver-master/src/ImpEncoder.cpp
+++ v4l2rtspserver-master/src/ImpEncoder.cpp
@@ -33,19 +33,16 @@
 
 #include "ImpEncoder.h"
 #include <stdexcept>
-//#include "logger.h"
+#include <map>
 
 // ---- OSD
 //
 
-//#include <time.h>
-
-// XXX: FreeType doesn't recommend hardcoding these paths
-#include "../freetype2/include/ft2build.h"
-#include "../freetype2/include/freetype/freetype.h"
-#include "../freetype2/include/freetype/ftglyph.h"
-#include "../freetype2/include/freetype/ftmodapi.h"
-#include "../freetype2/include/freetype/ftdriver.h"
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include FT_GLYPH_H
+#include FT_MODULE_H
+#include FT_DRIVER_H
 
 #include "sharedmem.h"
 #include "../../v4l2rtspserver-tools/sharedmem.h"
@@ -246,6 +243,8 @@ class OSD {
             if (IMP_OSD_SetRgnAttr(region, &attributes) != 0) {
                 throw std::runtime_error("Could not set boundary attributes");
             }
+
+            clear();
         }
 
         void clear() {
@@ -304,6 +303,18 @@ class OSD {
 
             return image[y * _width + x];
         }
+};
+
+struct CachedGlyphBitmap {
+    int width;
+    int height;
+
+    int offset_x;
+    int offset_y;
+
+    int baseline_offset;
+
+    uint32_t *data;
 };
 
 std::pair<int, int> get_vertical_font_dimensions(FT_Face &face) {
@@ -377,6 +388,17 @@ void osd_draw_timestamp(OSD &timestamp_osd, FT_Face &face, int baseline_offset, 
 
         int glyph_index = FT_Get_Char_Index(face, c);
 
+        /*
+        map<int, CachedGlyphBitmap>::iterator glyph_iterator = glyph_map.find(glyph_index);
+
+        if (glyph_iterator == m.end()) {
+            if (FT_Render_Glyph(glyph, FT_RENDER_MODE_NORMAL) != 0) {
+                LOG_S(INFO) << "Could not render glyph for character: " << c;
+                break;
+            }
+        }
+        */
+
         if (FT_Load_Glyph(face, glyph_index, FT_LOAD_DEFAULT) != 0) {
             LOG_S(INFO) << "Could not load glyph for character: " << c;
             break;
@@ -424,7 +446,7 @@ void osd_draw_timestamp(OSD &timestamp_osd, FT_Face &face, int baseline_offset, 
         }
 
         // Move the pen
-        pen.x += glyph->advance.x / 64;
+        pen.x += (glyph->advance.x / 64) + currentConfig.osdSpace;
         pen.y -= glyph->advance.y / 64;
     }
 
@@ -450,6 +472,8 @@ static void* update_thread(void *p) {
 
     FT_Library library;
     FT_Face face;
+    std::map< int, CachedGlyphBitmap> font_cache;
+
     int font_baseline_offset = 0;
     
     if (FT_Init_FreeType(&library) != 0) {
@@ -475,7 +499,7 @@ static void* update_thread(void *p) {
     OSD motion_osd = OSD(image_width - DETECTION_CIRCLE_SIZE, 0, DETECTION_CIRCLE_SIZE, DETECTION_CIRCLE_SIZE, 0);
 
     // Default to top left and 10px high until we read the config
-    OSD timestamp_osd = OSD(0, 0, image_width - 5, 100, 1);
+    OSD timestamp_osd = OSD(0, 0, image_width, 10, 1);
 
     if (IMP_OSD_Start(0) != 0) {
         LOG_S(ERROR) << "OSD show error";
@@ -570,6 +594,8 @@ static void* update_thread(void *p) {
         if (firstConfigPass || (currentConfig.osdFixedWidth != newConfig->osdFixedWidth)) {
             int result;
 
+            FT_Done_Face(face);
+
             if (newConfig->osdFixedWidth) {
                 result = FT_New_Face(library, OSD_FONT_MONO, 0, &face);
             } else {
@@ -615,11 +641,6 @@ static void* update_thread(void *p) {
             LOG_S(INFO) <<  "Done";
 
             LOG_S(INFO) << "Changed OSD size";
-        }
-
-        if (currentConfig.osdSpace != newConfig->osdSpace) {
-            // As the size changed, re-display the OSD
-            LOG_S(INFO) <<  "Changed OSD space";
         }
 
         if (currentConfig.motionTracking != newConfig->motionTracking ) {
