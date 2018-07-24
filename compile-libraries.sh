802159ecb76e22c928d0c5bd8b6dfb1008290ca2
puddly <32534428+puddly@users.noreply.github.com>
2018-07-24 14:31:14 -0400

    Updated scripts to build properly according to instructions in README

--- compile-libraries.sh
+++ compile-libraries.sh
@@ -1,19 +1,33 @@
-./lame/compile.sh
-./live/compile.sh
-cd opus
-./compile.sh
-cd ..
-
-cd freetype2
-TOOLCHAIN=$(pwd)/../toolchain/bin
+TOOLCHAIN=$(pwd)/toolchain/bin
 CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
-make clean
 export CC=${CROSS_COMPILE}gcc
 export CFLAGS="-muclibc -O3"
 export CPPFLAGS="-muclibc -O3"
 export LDFLAGS="-muclibc -O3"
 
+cd live
+./genMakefiles dafang
+make clean
+make
+make install
+cd ..
+
+cd opus
 ./autogen.sh
+./configure --host=mips-linux --prefix=$(pwd)/_install
+make
+make install
+cd ..
+
+cd lame
+./configure --host=mips-linux-gnu --prefix=$(pwd)/_install
+make libmp3lame.so
+make install
+cd ..
+
+cd freetype2
+./autogen.sh --host=mips-linux-gnu --without-harfbuzz --without-png --without-zlib --prefix=$(pwd)/_install
 ./configure --host=mips-linux-gnu --without-harfbuzz --without-png --without-zlib --prefix=$(pwd)/_install
 make install
 cd ..
+
